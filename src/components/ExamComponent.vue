b8d9545705c18574eb605df9b88f7391cb134387
Ahmed Shibob <ashibob.yousef@gmail.com>
2025-02-21 23:43:10 +0200

    Implement Vue Router and refactor exam components
    - Add Vue Router to project dependencies
    - Configure router in main.js
    - Update App.vue to use router-view
    - Refactor ExamComponent to use route query parameters
    - Enhance AudioComponent and QuestionComponent with improved logic
    - Add axios calls for exam flow management

--- src/components/ExamComponent.vue
+++ src/components/ExamComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container v-if="examType && studentId">
+  <v-container v-if="$route.query.examType && $route.query.studentId">
     <v-row justify="center">
       <v-col cols="12" md="8">
         <v-card class="pa-6">
@@ -22,21 +22,22 @@
           </v-btn>
           <div v-if="examStarted && !loading">
             <AudioComponent
-              v-if="isAudioLevel && !audioCompleted"
+              v-if="examConfig.is_audio_level && !audioCompleted"
               :audioFile="audioFile"
               :listeningToAudio="listeningToAudio"
+              :audioListeningCount="examConfig.audio_listening_count"
               @audio-ended="handleAudioEnded"
               @replay-audio="handleReplayAudio"
               @proceed-to-question="handleProceedToQuestion"
             />
             <QuestionComponent
-              v-if="!isAudioLevel || audioCompleted"
-              :question="currentQuestion"
-              :duration="questionDuration"
+              v-if="!examConfig.is_audio_level || audioCompleted"
+              :question="examQuestion"
+              :duration="examQuestion.question_duration || 8"
               @answer="handleAnswer"
               @time-up="handleTimeUp"
             />
-            <CounterComponent :current="questionNumber" :total="totalQuestions" />
+            <CounterComponent :current="examConfig.question_number" :total="examConfig.questions_count" />
             <EvaluationComponent v-if="examCompleted" :results="evaluationResults" />
           </div>
         </v-card>
@@ -46,7 +47,7 @@
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
 import QuestionComponent from './QuestionComponent.vue';
 import CounterComponent from './CounterComponent.vue';
 import EvaluationComponent from './EvaluationComponent.vue';
@@ -62,22 +63,115 @@ export default {
   data() {
     return {
       baseUrl: 'http://ftcourses.test/api',
-      studentId: null,
-      examType: null,
-      urlData: null,
       loading: false,
       examAttemptId: null,
+      examStarted: false,
+      examConfig: null,
+      examQuestion: null,
+      audioFile: null,
+      evaluationResults: null,
     };
   },
-
-  computed: {
-    queryParams() {
-      return this.$route;
+  watch: {
+    examConfig: {
+      immediate: true,
+      deep: true,
+      handler() {
+        if(this.examConfig.is_audio_level) { 
+          this.getAudioFile()
+        }
+      }
     }
   },
-  mounted() {
-    const queryParams = this.$route.query;
-console.log('Query Params:', queryParams);
+  methods: {
+    handleAudioEnded() {
+      this.getNextQuestion()
+    },
+    handleProceedToQuestion() {
+      this.getNextQuestion()
+    },
+    getAudioFile() {
+      axios.get(`${this.baseUrl}/exams/get-audio/${this.examConfig.exam_attempt_id}`, {
+        params: {
+          ...this.$route.query
+        }
+      }).then((response) => {
+        this.audioFile = response.data.data.audio_file;
+        this.examConfig = response.data.data.exam;
+      }).catch((error) => {
+        console.error('Error getting audio file:', error);
+      });
+    },
+    startExam() {
+      this.examStarted = true;
+      this.loading = true;
+
+      axios.post(`${this.baseUrl}/exams/start`, {
+        params: {
+          exam_type: this.$route.query.examType,
+          student_id: this.$route.query.studentId,
+        },
+      }).then((response) => {
+        this.examConfig = response.data.data.exam;
+        this.examQuestion = response.data.data;
+        this.loading = false;
+      }).catch((error) => {
+        console.error('Error starting exam:', error);
+        this.loading = false;
+      });
+    },
+    handleAnswer(answer) {
+
+      axios.post(`${this.baseUrl}/exams/submit-answer`, {
+        params: {
+         ...this.$route.query
+        },
+        body: {
+          answer: answer,
+          question_id: this.examQuestion.id,
+          exam_attempt_id: this.examConfig.exam_attempt_id
+        }
+      }).then((response) => {
+        this.examConfig = response.data.data.exam;
+        this.getNextQuestion()
+      }).catch((error) => {
+        console.error('Error submitting answer:', error);
+      });
+    },
+    handleTimeUp() {
+      this.handleAnswer('')
+    },
+    getNextQuestion() {
+      if (this.examConfig.question_number < this.examConfig.questions_count) {
+
+
+        axios.get(`${this.baseUrl}/exams/get-question/${this.examConfig.exam_attempt_id}`, {
+          params: {
+            ...this.$route.query
+          }
+        }).then((response) => {
+          this.examConfig = response.data.data.exam;
+          this.examQuestion = response.data.data;
+        }).catch((error) => {
+          console.error('Error getting next question:', error);
+        });
+      }
+      else {
+        this.examCompleted = true;
+        this.getEvaluationResults
+      }
+    },
+    getEvaluationResults() {
+      axios.get(`${this.baseUrl}/exams/evaluation-exam/${this.examConfig.exam_attempt_id}`, {
+        params: {
+          ...this.$route.query
+        }
+      }).then((response) => {
+        this.evaluationResults = response.data.data;
+      }).catch((error) => {
+        console.error('Error getting evaluation results:', error);
+      });
+    }
   }
 
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import vuetify from './plugins/vuetify';
-import 'vuetify/styles'; // Import Vuetify styles
+import { createApp } from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "vuetify/styles"; // Import Vuetify styles
+import router from "./router"; // Import the router
 
 const app = createApp(App);
 app.use(vuetify);
-app.mount('#app');
+app.use(router); // Use the router
+app.mount("#app");
